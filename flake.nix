ecb770532074d6af245e0bb0a57a70b305547823
Riccardo Casatta <riccardo@casatta.it>
2024-04-15 14:04:38 +0200

    init flake

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          inherit (pkgs) lib;
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+          src = craneLib.cleanCargoSource ./.;
+
+          nativeBuildInputs = with pkgs; [ rustToolchain ];
+          buildInputs = with pkgs; [ ];
+          commonArgs = {
+            inherit src buildInputs nativeBuildInputs;
+          };
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          bin = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts ;
+          });
+        in
+        with pkgs;
+        {
+          packages =
+            {
+              inherit bin;
+              default = bin;
+            };
+          devShells.default = mkShell {
+            inputsFrom = [ bin ];
+
+            buildInputs = with pkgs; [  ];
+          };
+        }
+      );
+}
