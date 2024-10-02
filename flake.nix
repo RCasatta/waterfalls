a14b60455434a643fb8cf3459c6ad123b639cce4
Riccardo Casatta <riccardo@casatta.it>
2024-10-02 17:12:32 +0200

    remove unused follows

--- flake.nix
+++ flake.nix
@@ -4,16 +4,10 @@
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     crane = {
       url = "github:ipetkov/crane";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
     };
   };
   outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
