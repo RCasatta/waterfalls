8a7cef2cb67fecaad4266fbcb555f59204b1bd21
Riccardo Casatta <riccardo@casatta.it>
2025-01-10 14:59:30 +0100

    use syslog style logging for systemd

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,7 @@ use waterfalls::server::{inner_main, Arguments};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let mut builder = env_logger::Builder::from_env(Env::default().default_filter_or("info"));
-    if std::env::var("LOG_AVOID_TIMESTAMP").is_ok() {
-        builder.format(|buf, record| {
-            writeln!(
-                buf,
-                "{:5} {} {}",
-                record.level(),
-                record.target(),
-                record.args()
-            )
-        });
-    }
-    builder.init();
+    init_logging();
 
     let args = Arguments::parse();
 
@@ -29,4 +17,24 @@ async fn shutdown_signal() {
     tokio::signal::ctrl_c()
         .await
         .expect("failed to install CTRL+C signal handler");
+}
+
+fn init_logging() {
+    let mut builder = env_logger::Builder::from_env(Env::default().default_filter_or("info"));
+    if let Ok(s) = std::env::var("RUST_LOG_STYLE") {
+        if s == "SYSTEMD" {
+            builder.format(|buf, record| {
+                let level = match record.level() {
+                    log::Level::Error => 3,
+                    log::Level::Warn => 4,
+                    log::Level::Info => 6,
+                    log::Level::Debug => 7,
+                    log::Level::Trace => 7,
+                };
+                writeln!(buf, "<{}>{}: {}", level, record.target(), record.args())
+            });
+        }
+    }
+
+    builder.init();
 }
