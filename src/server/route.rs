ed9fe9109acd5266632a878cdbb449b346ada957
Riccardo Casatta <riccardo@casatta.it>
2025-07-11 09:18:53 +0200

    defer vin/vout computation for mempool

--- src/server/route.rs
+++ src/server/route.rs
@@ -554,7 +554,7 @@ async fn find_scripts(
 
     for (conf, unconf) in seen_blockchain.iter_mut().zip(seen_mempool.iter()) {
         for txid in unconf {
-            conf.push(TxSeen::mempool(*txid, 0)) // TODOV
+            conf.push(TxSeen::mempool(*txid, 0)) // TODO: compute vin/vout also for mempool
         }
     }
     let is_last = seen_blockchain.iter().all(|e| e.is_empty());
