a20b53aaeeb2533b57cc50dc3fa0fc16afa2a00c
Riccardo Casatta <riccardo@casatta.it>
2025-08-01 10:45:17 +0200

    use a lru cache to limit cache size

--- src/server/derivation_cache.rs
+++ src/server/derivation_cache.rs
@@ -1,27 +1,26 @@
-use std::{
-    collections::HashMap,
-    hash::{DefaultHasher, Hash, Hasher},
-};
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+use lrumap::LruHashMap;
 
 use crate::{cache_counter, ScriptHash};
 
 pub type DescIndexHash = u64;
 
 pub struct DerivationCache {
-    cache: HashMap<DescIndexHash, ScriptHash>, // TODO use passthrough hasher
+    cache: LruHashMap<DescIndexHash, ScriptHash>, // TODO use passthrough hasher
 }
 
 impl DerivationCache {
     pub fn new() -> Self {
         Self {
-            cache: HashMap::default(),
+            cache: LruHashMap::new(1_000_000), // TODO make this configurable
         }
     }
     pub fn add(&mut self, x: DescIndexHash, script_pubkey: ScriptHash) {
         // TODO this should delete old entries to avoid growing indefinitely
-        self.cache.insert(x, script_pubkey);
+        self.cache.push(x, script_pubkey);
     }
-    pub fn get(&self, x: DescIndexHash) -> Option<ScriptHash> {
+    pub fn get(&mut self, x: DescIndexHash) -> Option<ScriptHash> {
         let val = self.cache.get(&x).cloned();
         let hit_miss = val.is_some();
         cache_counter("derivation_cache", hit_miss);
@@ -33,9 +32,5 @@ impl DerivationCache {
         desc.hash(&mut hasher);
         index.hash(&mut hasher);
         hasher.finish()
-    }
-
-    pub fn len(&self) -> usize {
-        self.cache.len()
     }
 }
