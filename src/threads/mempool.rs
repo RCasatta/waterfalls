92065c677dae238e9cf7f696cc617deb2335d31f
Riccardo Casatta <riccardo@casatta.it>
2025-08-14 09:45:12 +0200

    refactor out async block in mempool into a function

--- src/threads/mempool.rs
+++ src/threads/mempool.rs
@@ -18,6 +18,58 @@ pub(crate) async fn mempool_sync_infallible(
     }
 }
 
+async fn sync_mempool_once(
+    client: &Client,
+    support_verbose: bool,
+    mempool_txids: &mut HashSet<elements::Txid>,
+    state: &Arc<State>,
+    family: Family,
+) {
+    match client.mempool(support_verbose).await {
+        Ok(current) => {
+            let db = &state.store;
+            let tip = state.tip().await;
+            let new: Vec<_> = current.difference(&mempool_txids).collect();
+            let removed: Vec<_> = mempool_txids.difference(&current).cloned().collect();
+            if !new.is_empty() {
+                log::debug!("new txs in mempool {:?}, tip: {tip:?}", new);
+            }
+            if !removed.is_empty() {
+                log::info!(
+                    "removed {} txs from mempool, tip: {tip:?}, still in mempool: {}",
+                    removed.len(),
+                    current.len()
+                );
+            }
+
+            let mut txs = vec![];
+            for new_txid in new {
+                match client.tx(*new_txid, family).await {
+                    Ok(tx) => txs.push(tx),
+                    Err(e) => {
+                        if let Some(crate::fetch::Error::TxNotFound(_, _)) = e.downcast_ref() {
+                            // tx not found, it was replaced from mempool with RBF for example
+                            log::info!("{e}");
+                        } else {
+                            log::error!("{e}");
+                        }
+                    }
+                }
+            }
+            {
+                let mut m = state.mempool.lock().await;
+                m.remove(&removed);
+                m.add(db, &txs);
+                *mempool_txids = m.txids();
+            }
+        }
+        Err(e) => {
+            log::warn!("mempool sync error, is the node running and has rest=1 ?\n{e:?}")
+        }
+    }
+    sleep(std::time::Duration::from_secs(1)).await;
+}
+
 async fn mempool_sync(
     state: Arc<State>,
     client: Client,
@@ -43,7 +95,6 @@ async fn mempool_sync(
         }
     }
 
-    let db = &state.store;
     let mut mempool_txids = HashSet::new();
     let support_vebose = client.mempool(true).await.is_ok();
     log::info!("mempool support verbose: {support_vebose}");
@@ -55,48 +106,7 @@ async fn mempool_sync(
                 return Ok(());
             }
             _ = async {
-                match client.mempool(support_vebose).await {
-                    Ok(current) => {
-                        let tip = state.tip().await;
-                        let new: Vec<_> = current.difference(&mempool_txids).collect();
-                        let removed: Vec<_> = mempool_txids.difference(&current).cloned().collect();
-                        if !new.is_empty() {
-                            log::debug!("new txs in mempool {:?}, tip: {tip:?}", new);
-                        }
-                        if !removed.is_empty() {
-                            log::info!(
-                                "removed {} txs from mempool, tip: {tip:?}, still in mempool: {}",
-                                removed.len(),
-                                current.len()
-                            );
-                        }
-
-                        let mut txs = vec![];
-                        for new_txid in new {
-                            match client.tx(*new_txid, family).await {
-                                Ok(tx) => txs.push(tx),
-                                Err(e) => {
-                                    if let Some(crate::fetch::Error::TxNotFound(_, _)) = e.downcast_ref() {
-                                        // tx not found, it was replaced from mempool with RBF for example
-                                        log::info!("{e}");
-                                    } else {
-                                        log::error!("{e}");
-                                    }
-                                }
-                            }
-                        }
-                        {
-                            let mut m = state.mempool.lock().await;
-                            m.remove(&removed);
-                            m.add(db, &txs);
-                            mempool_txids = m.txids();
-                        }
-                    }
-                    Err(e) => {
-                        log::warn!("mempool sync error, is the node running and has rest=1 ?\n{e:?}")
-                    }
-                }
-                sleep(std::time::Duration::from_secs(1)).await;
+                sync_mempool_once(&client, support_vebose, &mut mempool_txids, &state, family).await;
             } => {}
         }
     }
