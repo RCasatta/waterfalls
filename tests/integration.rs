2789de0bc106d56dfb3e249d85ad795ead3e0fd2
Riccardo Casatta <riccardo@casatta.it>
2025-08-26 11:51:01 +0200

    fix url in a couple of ignored tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -666,7 +666,7 @@ async fn test_lwk_wollet_huge_testnet() {
     do_lwk_scan(
         lwk_wollet::ElementsNetwork::LiquidTestnet,
         "ct(slip77(1bda6cd71a1e206e3eb793e5a4d98a46c3fa473c9ab7bdef9bb9c814764d6614),elwpkh([cb4ba44a/84'/1'/0']tpubDDrybtUajFcgXC85rvwPsh1oU7Azx4kJ9BAiRzMbByqK7UnVXY3gDRJPwEDfaQwguNUZFzrhavJGgEhbsfuebyxUSZQnjLezWVm2Vdqb7UM/<0;1>/*))#za9ktavp",
-        "https://waterfalls.liquidwebwallet.org/liquidtestnet-utxo-only/api/",
+        "https://waterfalls.liquidwebwallet.org/liquidtestnet/api/",
         9361396473,
     )
     .await;
@@ -680,7 +680,7 @@ async fn test_lwk_wollet_small_testnet() {
     do_lwk_scan(
         lwk_wollet::ElementsNetwork::LiquidTestnet,
         "ct(slip77(ac53739ddde9fdf6bba3dbc51e989b09aa8c9cdce7b7d7eddd49cec86ddf71f7),elwpkh([93970d14/84'/1'/0']tpubDC3BrFCCjXq4jAceV8k6UACxDDJCFb1eb7R7BiKYUGZdNagEhNfJoYtUrRdci9JFs1meiGGModvmNm8PrqkrEjJ6mpt6gA1DRNU8vu7GqXH/<0;1>/*))#u0y4axgs",
-        "https://waterfalls.liquidwebwallet.org/liquidtestnet-utxo-only/api/",
+        "https://waterfalls.liquidwebwallet.org/liquidtestnet/api/",
         1070521,
     )
     .await;
