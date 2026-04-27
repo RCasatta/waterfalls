1f045aabea455c6d3e1f18e32170ff92b5c12685
Riccardo Casatta <riccardo@casatta.it>
2026-04-27 15:43:30 +0200

    check status in test_env fn

--- src/test_env.rs
+++ src/test_env.rs
@@ -530,9 +530,12 @@ impl WaterfallClient {
             .send()
             .await?;
 
+        let status = response.status().as_u16();
         let headers = response.headers().clone();
-
         let body = response.text().await?;
+        if status != 200 {
+            bail!("waterfalls response is not 200 but: {status} body is: {body}");
+        }
         Ok((serde_json::from_str(&body)?, headers))
     }
 
